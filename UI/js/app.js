b1fd8db830b691268ff968d2b8318d73915b2d0d
bedann <bedank6@gmail.com>
2019-02-20 22:40:09 +0300

    [Feature #164113353] loader

--- UI/js/app.js
+++ UI/js/app.js
@@ -6,7 +6,7 @@
  /**
  * Convinience methods and properties
  */
-const BASE_URL = 'http://127.0.0.1:5000/api/v2';
+const BASE_URL = 'https://kurayangu.herokuapp.com/api/v2';
 
 function get_token(){
     token = localStorage.getItem('token');
@@ -22,6 +22,9 @@ function get_token(){
  * Login function
  */
 function on_login() {
+    loader = document.getElementById('load-modal');
+    loader.style.display = 'block';
+
     fetch(`${BASE_URL}/auth/login`, {
         method: 'POST',
         headers: {
@@ -34,21 +37,46 @@ function on_login() {
     })
     .then(res => res.json())
     .then((data) => {
+        loader.style.display = 'none';
 
         if (data.status === 200) {
+            var user = data.data[0].user
 
+            // Save user profile to local storage
             localStorage.setItem('token', data.data[0].token);
-            localStorage.setItem('logged_in', true);
+            localStorage.setItem('firstname', user.firstname);
+            localStorage.setItem('lastname', user.lastname);
+            localStorage.setItem('email', user.email);
+            localStorage.setItem('phone', user.phonenumber);
             // Redirect to homepage after successful login
             window.location.replace('index.html');
 
         }else {
             window.alert(data.error);
+            console.log(data.status);
         }
 
     })
     .catch((error) => {
+        loader.style.display = 'none';
         window.alert(error);
     });
 }
 
+
+
+/** 
+* PROFILE PAGE
+*/
+function loadUserProfile(){
+    fname = localStorage.getItem('firstname');
+    lname = localStorage.getItem('lastname');
+    document.getElementById('username').innerText = `${fname} ${lname}`
+    document.getElementById('email').innerText = localStorage.getItem('email')
+    document.getElementById('phone').innerText = localStorage.getItem('phone')
+}
+
+function on_logout(){
+    localStorage.clear();
+    window.location.replace('signup.html')
+}
